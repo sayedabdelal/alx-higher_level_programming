9872747aee793daf6a108980bcafccbf4a9fbcb2
sayedabdelal <saidmo19734@gmail.com>
2023-06-08 23:10:51 -0700

    clne

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz ", end="")
+    elif i % 3 == 0:
+        print("Fizz ", end="")
+    elif i % 5 == 0:
+        print("Buzz ")
+    else:
+        print("{} ".format(i), end="")
