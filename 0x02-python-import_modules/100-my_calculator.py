b6221335b31c47418bb8f95563c90a550f96fdf5
sayedabdelal <saidmo19734@gmail.com>
2023-06-11 06:35:07 -0700

    chore

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ def main():
     from sys import argv
 
     len_argv = len(argv) - 1
-    id = 1
+    operators = {'+', '*', '/', '-'}
     if len_argv != 3:
         print(" Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
@@ -12,18 +12,23 @@ def main():
     b = int(argv[3])
     opr = argv[2]
     # check operator exists or not
-    if opr == '+':
-        value = add(a, b)
-    elif opr == '-':
-        value = sub(a, b)
-    elif opr == '*':
-        value = mul(a, b)
-    elif opr == '/':
-        value = div(a, b)
-    else:
+    if opr not in operators:
         # operator not exists
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+    else:
+        if opr == '+':
+            value = add(a, b)
+        elif opr == '-':
+            value = sub(a, b)
+        elif opr == '*':
+            value = mul(a, b)
+        elif opr == '/':
+            value = div(a, b)
+        else:
+            # operator is not supported
+            print("Unsupported operator:", opr)
+            exit(1)
     print(value)
 
 
