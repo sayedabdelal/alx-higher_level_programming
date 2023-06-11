b3e6ad9762b8b9c61440b96cb1044487011764df
sayedabdelal <saidmo19734@gmail.com>
2023-06-11 00:25:03 -0700

    chone

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-value = add(a, b)
-print(f"{a} + {b} = {value}")
+
+
+def main():
+    a = 1
+    b = 2
+    value = add(a, b)
+    print(f"{a} + {b} = {value}")
+
+
+if __name__ == "__main__":
+    main()
