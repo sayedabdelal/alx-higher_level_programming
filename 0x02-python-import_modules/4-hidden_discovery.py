7b1d90040d7c381d29fac6b6b4096dc294ae05b6
sayedabdelal <saidmo19734@gmail.com>
2023-06-11 03:47:13 -0700

    chore

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-import hidden_4
+def main():
+    import hidden_4
 
-# Get the list of names defined in the module
-module_names = dir(hidden_4)
-# Iterate over each name in the module_names list
-for name in module_names:
-    # Check if the name starts with an underscore
-    if not name.startswith('_'):
-        print(name)
+    # Get the list of names defined in the module
+    module_names = dir(hidden_4)
+    # Iterate over each name in the module_names list
+    for name in module_names:
+        # Check if the name starts with an underscore
+        if not name.startswith("__"):
+            print(name)
+if __name__ == "__main__":
+    main()
