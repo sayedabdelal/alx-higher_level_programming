be994fff9306094d66408bc5bf3c0fb9ec1c6286
sayedabdelal <saidmo19734@gmail.com>
2023-06-14 12:17:46 -0700

    final_fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 def main():
     from calculator_1 import add, sub, mul, div
-    from sys import argv
+    import sys
 
-    len_argv = len(argv) - 1
-    operators = {'+': add, '*': mul, '/': div, '-': sub}
-    if len_argv != 3:
-        print(" Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    opr = argv[2]
-    # check operator exists or not
-    if opr not in operators:
-        # operator not exists
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-    print("{} {} {} = {}".format(a, opr, b, operators[opr](a, b)))
+        exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
 
 
 if __name__ == "__main__":
