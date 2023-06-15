954cb10a009cad8491cfaba1f1bb3bea70e54db4
sayedabdelal <saidmo19734@gmail.com>
2023-06-15 06:04:41 -0700

    chore

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for k in a_dictionary:
-        if k == key:
-            a_dictionary[k] = value
-        else:
-            a_dictionary[key] = value
-
-        return a_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
