3ddf46fa014fd4cc5ebaff344cbe37f58acb1e86
sayedabdelal <saidmo19734@gmail.com>
2023-06-26 09:20:23 -0700

    chore

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for ele in my_list:
+            if count == x:
+                break
+            if isinstance(ele, int):
+                print("{:d}".format(ele), end="")
+                count += 1
+    except TypeError:
+        pass
+    except IndexError:
+        pass
+    finally:
+        print()
+        return count
