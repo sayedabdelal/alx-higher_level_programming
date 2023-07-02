dea0cd391ba27a31e793364bb3981c0b09166fc3
sayedabdelal <saidmo19734@gmail.com>
2023-07-02 06:11:54 -0700

    chore

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """apperar  a square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        size - represnets the size of the sq
+        """
+
+        self.__size = size
