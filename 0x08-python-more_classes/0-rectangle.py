2d121d3acfe83ceb995c366feaf45bcc301b8837
sayedabdelal <saidmo19734@gmail.com>
2023-07-03 13:06:26 -0700

    chore

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+    Module: 0-rectangle
+    Description: This module defines the Rectangle class.
+"""
+
+
+
 class Rectangle:
+    """
+        appear a Rectangle    
+    """
     pass
