15af6a675fa59bad699189ecf3028d6f9cd25012
sayedabdelal <saidmo19734@gmail.com>
2023-07-11 10:13:40 -0700

    chore

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -12,9 +12,9 @@ class Student:
             last_name (str): The last name of the student.
             age (int): The age of the student.
         """
-        first_name = self.first_name
-        last_name = self.last_name
-        age = self.age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """Gets a dictionary representation of the Student"""
