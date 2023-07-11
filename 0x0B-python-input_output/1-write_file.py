3791fda6485bad8a473eb4d79e57781134d8cb50
sayedabdelal <saidmo19734@gmail.com>
2023-07-11 05:08:04 -0700

    chore

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """write a string to a text file and return number of chr """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
