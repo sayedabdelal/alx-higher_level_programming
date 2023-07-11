27ee8888271cf5965f970679e11e8ee6fd3bb476
sayedabdelal <saidmo19734@gmail.com>
2023-07-11 03:29:57 -0700

    chore

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """ This Read text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
