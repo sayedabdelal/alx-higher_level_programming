52975ce1f0f1eb726d4a368d83879b8075b2ee7f
sayedabdelal <saidmo19734@gmail.com>
2023-07-11 10:12:08 -0700

    chore

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,7 +5,13 @@
 class Student:
     """Create a student."""
     def __init__(self, first_name, last_name, age):
-        """Create a new student"""
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         first_name = self.first_name
         last_name = self.last_name
         age = self.age
