e3d5ff66e47a8435523749d2c9d7808bb520f60e
sayedabdelal <saidmo19734@gmail.com>
2023-07-11 11:29:54 -0700

    chore

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -16,7 +16,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Gets a dictionary representation of the Student"""
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
