837e51f56aea769c4f607bab47199faa8c8338a8
sayedabdelal <saidmo19734@gmail.com>
2023-07-11 05:06:50 -0700

    chore

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """write a string to a text file and return number of chr """
+    with open(filename, encoding="utf-8") as f:
+        return f.write(text)
