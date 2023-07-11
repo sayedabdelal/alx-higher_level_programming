ff1178e9bc806d5c2223fa627aef4a242ed07784
sayedabdelal <saidmo19734@gmail.com>
2023-07-11 10:06:36 -0700

    chore

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module defines a class Student"""
+
+
+class Student:
+    """Create a student."""
+    
+    
+    def __init__(self, first_name, last_name, age):
+        """Create a new student"""
+        
+        
+        first_name = self.first_name
+        last_name = self.last_name
+        age = self.age
+        
+    def to_json(self):
+        """Gets a dictionary representation of the Student"""
+        self.__dict__
