5a9ed7121179b7b4b28eccf772a16db91228feee
sayedabdelal <saidmo19734@gmail.com>
2023-08-17 04:28:02 -0700

    chore

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+--  lists all records of the table second_tablE
+SELECT score, name
+FROM hbtn_0c_0.second_table
+ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+--  lists all records with a score >= 10 in the table 
+SELECT score, name
+FROM hbtn_0c_0.second_table
+WHERE score >= 10
+ORDER BY score DESC;
